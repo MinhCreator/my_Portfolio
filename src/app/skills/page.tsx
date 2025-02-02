a347f1c2ba2dfcd9e1ec630a2e9c44adf84c38c4
MinhCreator <92376650+MinhCreator@users.noreply.github.com>
2025-02-02 15:05:38 +0700

    update website

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,43 +2,41 @@ import Heading from "@/components/Heading";
 import SkillsFooter from "@/components/SkillsFotter";
 import { Badge } from "@/components/ui/badge";
 import { Lightbulb } from "lucide-react";
-import html from '../../../public/html.png'
-import css from '../../../public/css.png'
-import scss from '../../../public/scss.png'
-import js from '../../../public/js.png'
-import ts from '../../../public/ts.png'
-import react from '../../../public/react.png'
-import nextjs from '../../../public/nextjs.jpeg'
-import cplus from '../../../public/c++.jpeg'
-import tailwind from '../../../public/tailwindcss.png'
-import github from '../../../public/github.png'
-import vscode from '../../../public/vs.png'
-import vc from '../../../public/git.png'
-import lua from '../../../public/lua.png'
-import gd from '../../../public/godot.png'
-import python from '../../../public/python.png'
+import html from "../../../public/html.png";
+import css from "../../../public/css.png";
+import scss from "../../../public/scss.png";
+import js from "../../../public/js.png";
+import ts from "../../../public/ts.png";
+import react from "../../../public/react.png";
+import nextjs from "../../../public/nextjs.jpeg";
+import cplus from "../../../public/c++.jpeg";
+import tailwind from "../../../public/tailwindcss.png";
+import github from "../../../public/github.png";
+import vscode from "../../../public/vs.png";
+import vc from "../../../public/git.png";
+import lua from "../../../public/lua.png";
+import gd from "../../../public/godot.png";
+import python from "../../../public/python.png";
 import FramerWrapper from "@/components/animation/FramerWrapper";
 const skillPage = () => {
-
-    const language = [
-        {alt:"html", img:html},
-        {alt:"css", img:css},
-        {alt:"js", img:js},
-        {alt:"ts", img:ts},
-        {alt:"godot", img:gd},
-        {alt:"python", img:python},
-        {alt:"lua", img:lua},
-    ]
-    const framework = [
-        {alt:"react", img:react},
-        {alt:"nextjs", img:nextjs},
-        {alt:"scss", img:scss},
-        {alt:"tailwind", img:tailwind},
-        {alt:"github", img:github},
-        {alt:"vscode", img:vscode},
-        {alt:"git", img:vc},
-    ]
-       
+  const language = [
+    { alt: "html", img: html },
+    { alt: "css", img: css },
+    { alt: "js", img: js },
+    { alt: "ts", img: ts },
+    { alt: "godot", img: gd },
+    { alt: "python", img: python },
+    { alt: "lua", img: lua },
+  ];
+  const framework = [
+    { alt: "react", img: react },
+    { alt: "nextjs", img: nextjs },
+    { alt: "scss", img: scss },
+    { alt: "tailwind", img: tailwind },
+    { alt: "github", img: github },
+    { alt: "vscode", img: vscode },
+    { alt: "git", img: vc },
+  ];
 
   return (
     // SKILLS PAGE
@@ -52,10 +50,10 @@ const skillPage = () => {
         <FramerWrapper y={0} x={200}>
           <p className="font-poppins text-xl w-full text-primary max-sm:text-lg">
             Currently i am a junior and i have a solid understand of Lua, HTML5,
-            CSS3, JS, GDscript, React and Python, including responsive design
-            principles. Moveover, my game project based on Godot Engine . I
-            specialize in building web applications and sites using Javascript,
-            Typescript, React, Nextjs & Node.
+            CSS3, JavaScript, GDscript, React and Python, including responsive
+            design principles. Moveover, my game project based on Godot Engine .
+            I specialize in building web applications and sites using
+            Javascript, Typescript, React, Nextjs & Node.
           </p>
         </FramerWrapper>
         <FramerWrapper y={100} delay={0.3} className="block">
@@ -67,13 +65,13 @@ const skillPage = () => {
               src="http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=MinhCreator&theme=2077"
               alt="language"
               height="300px"
-              width ="450px"
+              width="450px"
             />
             <img
               src="https://github-readme-stats.vercel.app/api/top-langs/?username=MinhCreator&layout=donut&count_private=true&show_icons=true&theme=radical&hide_border=false"
               alt=""
               height="298px"
-              width ="450px"
+              width="450px"
             />
             {/* <SkillsFooter items={language} /> */}
           </div>
@@ -82,7 +80,7 @@ const skillPage = () => {
           <h1 className="gap-2 text-2xl font-poppins text-primary font-semibold flex text_underline relative max-sm:text-xl">
             Framework/Libraries and Version Control
           </h1>
-          <div className="w-full h-fit flex-row flex justify-between items-center">
+          <div className="w-full h-fit flex-row flex justify-between gap-x-10 gap-y-5 items-center flex-wrap">
             <SkillsFooter items={framework} />
           </div>
         </FramerWrapper>
--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,21 +1,20 @@
-import logo from "../../public/portfolioLogo.png";
-import Image from "next/image"
+// import logo from "../../public/person_img.jpg";
+import logo from "../../public/Coding.gif";
+import Image from "next/image";
 
-const HeroImage = ()  => {
-
-
-
-    return(
-        <>
-          <Image
-          src={logo}
-          alt="logo"
-          loading="eager"
-          priority
-          height={1000}
-          width={1000}
-        />
-        </>
-    )
-}
-export default HeroImage
+const HeroImage = () => {
+  return (
+    <>
+      <Image
+        style={{ borderRadius: "20px" }}
+        src={logo}
+        alt="logo"
+        loading="eager"
+        priority
+        height={1000}
+        width={1000}
+      />
+    </>
+  );
+};
+export default HeroImage;
